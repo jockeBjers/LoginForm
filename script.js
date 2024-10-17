5717fe12037be28439b359c58d4d5928974ffdc5
Joakim Bjerselius <joakim.bjerselius@hotmail.com>
2024-10-17 21:04:29 +0200

    add shadow on hover on the popup

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function formPopUp() {
     document.getElementById("form-popup").style.display = "flex";
-    document.getElementById("open-button").style.display = "none"; // Hide the open button
+    document.getElementById("open-button").style.display = "none";
 }
 
-// Function to close the form
 function closeForm() {
-    document.getElementById("form-popup").style.display = "none"; // Hide the form
-    document.getElementById("open-button").style.display = "block"; // Show the open button
+    document.getElementById("form-popup").style.display = "none"; 
+    document.getElementById("open-button").style.display = "block";
 }
