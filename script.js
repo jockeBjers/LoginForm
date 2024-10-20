b62229ea473af87762b3fc2bfae58cd42079e7b2
Joakim Bjerselius <176925453+jockeBjers@users.noreply.github.com>
2024-10-20 07:09:32 +0200

    add more pages and javascript logic

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-function formPopUp() {
+function formPopUp() { 
     document.getElementById("form-popup").style.display = "flex";
     document.getElementById("open-button").style.display = "none";
+    showStep('step-1'); 
 }
 
 function closeForm() {
     document.getElementById("form-popup").style.display = "none"; 
     document.getElementById("open-button").style.display = "block";
+}
+
+function showStep(stepId) {
+    const steps = document.getElementsByClassName("form-step");
+    for (let step of steps) {
+        step.style.display = "none"; 
+    }
+    document.getElementById(stepId).style.display = "block"; 
 }
