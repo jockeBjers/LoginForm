787d83d0485aad837951c399ab58997cfd77fee2
Joakim Bjerselius <176925453+jockeBjers@users.noreply.github.com>
2024-10-20 10:29:16 +0200

    Add changes

--- script.js
+++ script.js
@@ -6,5 +6,15 @@ function formPopUp() {
 
 function closeForm() {
     document.getElementById("form-popup").style.display = "none"; 
-    document.getElementById("open-button").style.display = "block";
+    document.getElementById("open-button").style.display = "flex";
+
+}
+
+function showStep(stepId) {
+    const steps = document.getElementsByClassName("form-step");
+    for (let step of steps) {
+        step.style.display = "none"; 
+    }
+    document.getElementById(stepId).style.display = "flex"; 
+
 }
